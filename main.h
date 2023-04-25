9f2e6f97a59377a0c7c411b927029af80ea34186
Codesbymeng <a.menguit@gmail.com>
2023-04-26 01:01:33 +0200

    test

--- main.h
+++ main.h
@@ -5,27 +5,10 @@
 #include <stddef.h>
 
 int _putchar(char c);
+int _putstr(char *str);
+int _putint(int n);
+int _putuint(unsigned int n, int base, char *digits);
 int _printf(const char *format, ...);
 
-/* Functions for handling different format specifiers */
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
-int print_int(va_list args);
-int print_unsigned(va_list args);
-int print_octal(va_list args);
-int print_hex(va_list args);
-int print_hex_upper(va_list args);
-int print_binary(va_list args);
-
-/* Utility function */
-int num_len(unsigned int num, int base);
-
-/* Struct to hold a format specifier and its corresponding function */
-typedef struct format_specifier
-{
-    char spec;
-    int (*func)(va_list);
-} format_t;
 
 #endif /* MAIN_H */
