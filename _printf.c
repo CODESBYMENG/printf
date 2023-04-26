b08e2330178d6ac523227abbb00982ae8ca6e41a
Codesbymeng <a.menguit@gmail.com>
2023-04-26 03:38:17 +0200

    test

--- _printf.c
+++ _printf.c
@@ -1,201 +1,62 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 /**
- * _putchar - Writes a character to stdout.
- * @c: The character to write.
- *
- * Return: The number of bytes written.
- */
-int _putchar(char c)
-{
-  return write(1, &c, 1);
-}
-
-/**
- * _putstr - Writes a string to stdout.
- * @str: The string to write.
- *
- * Return: The number of bytes written.
- */
-int _putstr(char *str)
-{
-  int len = 0;
-
-  while (*str)
-  {
-    _putchar(*str++);
-    len++;
-  }
-
-  return len;
-}
-
-/**
- * _putint - Writes an integer to stdout.
- * @n: The integer to write.
- *
- * Return: The number of digits written.
- */
-int _putint(int n)
-{
-  if (n == 0)
-  {
-    return _putchar('0');
-  }
-
-  if (n < 0)
-  {
-    _putchar('-');
-    n = -n;
-  }
-
-  int digits = 0;
-  int temp = n;
-
-  while (temp)
-  {
-    digits++;
-    temp /= 10;
-  }
-
-  int divisor = 1;
-
-  for (int i = 1; i < digits; i++)
-  {
-    divisor *= 10;
-  }
-
-  while (divisor)
-  {
-    _putchar(n / divisor % 10 + '0');
-    divisor /= 10;
-  }
-
-  return digits;
-}
-
-/**
- * _putuint - Writes an unsigned integer to stdout in the specified base.
- * @n: The integer to write.
- * @base: The base to use for conversion.
- * @digits: The string of digits for the specified base.
- *
- * Return: The number of digits written.
- */
-int _putuint(unsigned int n, int base, char *digits)
-{
-  if (n == 0)
-  {
-    return _putchar('0');
-  }
-
-  int len = 0;
-
-  while (n)
-  {
-    int digit = n % base;
-    _putchar(digits[digit]);
-    n /= base;
-    len++;
-  }
-
-  return len;
-}
-
-/**
- * _printf - Writes formatted output to stdout.
- * @format: The format string.
- * @...: The optional arguments to format and write.
- *
- * Return: The number of characters written.
+ * _printf - Produces output according to a format.
+ * @format: A character string composed of zero or more directives
+ * that produces output when the directive is encountered.
+ * Return: The number of characters printed (excluding the null byte used
+ * to end output to strings).
  */
 int _printf(const char *format, ...)
 {
-  va_list args;
-  va_start(args, format);
-  int len = 0;
+    va_list args;
+    int count = 0, i = 0, j = 0;
+    char *str_arg, ch;
+    int int_arg;
 
-  while (*format)
-  {
-    if (*format == '%')
+    va_start(args, format);
+
+    while (format && format[i])
     {
-      format++;
-
-      switch (*format)
-      {
-        case 'c':
+        if (format[i] == '%')
         {
-          char c = va_arg(args, int);
-          _putchar(c);
-          len++;
-          break;
+            i++;
+            switch (format[i])
+            {
+                case 'c':
+                    ch = (char)va_arg(args, int);
+                    count += printf("%c", ch);
+                    break;
+                case 's':
+                    str_arg = va_arg(args, char *);
+                    if (str_arg == NULL)
+                        str_arg = "(null)";
+                    count += printf("%s", str_arg);
+                    break;
+                case '%':
+                    count += printf("%%");
+                    break;
+                case 'd':
+                case 'i':
+                    int_arg = va_arg(args, int);
+                    count += printf("%d", int_arg);
+                    break;
+                default:
+                    count += printf("%%");
+                    count += printf("%c", format[i]);
+                    break;
+            }
         }
-
-        case 's':
+        else
         {
-          char *s = va_arg(args, char *);
-          len += _putstr(s);
-          break;
-        }
-
-        case '%':
-        {
-          _putchar('%');
-          len++;
-          break;
-        }
-
-        case 'd':
-        case 'i':
-        {
-          int n = va_arg(args, int);
-          len += _putint(n);
-          break;
-        }
-
-        case 'b':
-        {
-          unsigned int n = va_arg(args, unsigned int);
-          len += _putuint(n, 2, "01");
-          break;
+            count += printf("%c", format[i]);
         }
 
-        case 'u':
-        {
-          unsigned int n = va_arg(args, unsigned int);
-          len += _putuint(n, 10, "0123456789");
-          break;
-        }
-        case 'o': {
-          unsigned int n = va_arg(args, unsigned int);
-          len += _putuint(n, 8, "01234567");
-          break;
-        }
-        case 'x': {
-          unsigned int n = va_arg(args, unsigned int);
-          len += _putuint(n, 16, "0123456789abcdef");
-          break;
-        }
-        case 'X': {
-          unsigned int n = va_arg(args, unsigned int);
-          len += _putuint(n, 16, "0123456789ABCDEF");
-          break;
-        }
-        default: {
-          _putchar(*format);
-          len++;
-          break;
-        }
-      }
-    } else {
-      _putchar(*format);
-      len++;
+        i++;
     }
-    format++;
-  }
-  va_end(args);
-  return len;
-}       
+
+    va_end(args);
+
+    return count;
+}
--- main.h
+++ main.h
@@ -1,14 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <stddef.h>
-
-int _putchar(char c);
-int _putstr(char *str);
-int _putint(int n);
-int _putuint(unsigned int n, int base, char *digits);
 int _printf(const char *format, ...);
-
 
 #endif /* MAIN_H */
